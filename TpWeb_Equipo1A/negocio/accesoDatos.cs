749712bbf2f0481ef3fcdac963d82b8834048c1f
LeonelGrosse <leonelgrosse@outlook.com>
2024-09-30 16:55:45 -0300

    Acceso a base de datos

---
+++ TpWeb_Equipo1A/negocio/accesoDatos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace negocio
+{
+    public class accesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public accesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database = PROMOS_DB; integrated security = true");
+            comando = new SqlCommand();
+        }
+
+        public void setConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void setParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void cerrarConexion()
+        {
+            if(lector != null)
+            {
+                lector.Close();
+            }
+            conexion.Close();
+        }
+    }
+}
