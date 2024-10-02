fa97b25a9fa07cb4b97353f8835ff4e2c6e8df3e
Santiago Lapeña <slapena88@gmail.com>
2024-10-02 19:06:07 +0200

    Agregado de voucher negocio, falta terminar

---
+++ TpWeb_Equipo1A/negocio/voucherNegocio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class voucherNegocio
+    {
+        List<voucher> lista = new List<voucher>();
+        accesoDatos datos = new accesoDatos();
+
+        public List<voucher> Listar()
+        {
+
+            string select = "";
+            string from = "";
+            string where = "";
+
+            try
+            {
+                datos.setConsulta(select + from + where);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    voucher aux = new voucher();
+
+                    lista.Add(aux);
+
+                }
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+
+    }
+}
