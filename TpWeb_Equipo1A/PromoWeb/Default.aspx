49dc6afa4d153b03bd1cfcefb3710179062464a9
LeonelGrosse <leonelgrosse@outlook.com>
2024-10-04 18:12:58 -0300

    Validacion voucher y otras cosas

--- TpWeb_Equipo1A/PromoWeb/Default.aspx
+++ TpWeb_Equipo1A/PromoWeb/Default.aspx
@@ -1,5 +1,21 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PromoWeb.Default" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col">
+
+            <div class="mb-3">
+                <label for="txtVoucher" class="form-label">INGRESE EL CODIGO DE SU VOUCHER</label>
+                <asp:TextBox runat="server" ID="txtVoucher" CssClass="form-control" type="text" />
+            </div>
+            <asp:Button Text="CANJEAR" CssClass="btn btn-primary" ID="btnCanjear" OnClick="btnCanjear_Click" runat="server" />
+
+        </div>
+        <div class="lblTexto">
+            <asp:Label ID="lblTexto" runat="server" Text=""></asp:Label>
+        </div>
+    </div>
 </asp:Content>
--- TpWeb_Equipo1A/PromoWeb/Default.aspx.cs
+++ TpWeb_Equipo1A/PromoWeb/Default.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using negocio;
+using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,35 @@ namespace PromoWeb
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void btnCanjear_Click(object sender, EventArgs e)
+        {
+            voucherNegocio negocio = new voucherNegocio();
+            voucher voucher = new voucher();
+            List<voucher> voucherList = new List<voucher>();
+            try
+            {
+                string cod = txtVoucher.Text;
+                voucherList = negocio.Listar(cod);
+                if (voucherList.Count != 0)
+                voucher = (voucherList[0]);
+                if (voucher.codigo == null)
+                {
+                    lblTexto.Text = "Codigo incorrecto. Ingreselo nuevamente";
+                }
+                else if(voucher.fechaCanje.Year != 0001)
+                {
+                    lblTexto.Text = "El codigo ya fue canjeado. Ingrese otro codigo";
+                }
+                else
+                {
+                    Response.Redirect("CatalogoPremios.aspx?codigo=" + txtVoucher.Text, false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Redirect("Error.aspx", false);
+            }
         }
     }
 }
