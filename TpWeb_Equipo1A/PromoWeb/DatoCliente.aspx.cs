be321c9daa385d995b52ebc5c0f18b35ebdd2804
JorgeMelgarejo1 <jorge_melgarejo_2015@hotmail.com>
2024-10-05 14:07:31 -0300

    Se mejoro la validacion de cliente

--- TpWeb_Equipo1A/PromoWeb/DatoCliente.aspx.cs
+++ TpWeb_Equipo1A/PromoWeb/DatoCliente.aspx.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,7 +13,10 @@ namespace PromoWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if(!IsPostBack)
+            {
+                lblErrorDireccion.Text = string.Empty;
+            }
         }
 
         protected void ButtonParticipar_Click(object sender, EventArgs e)
@@ -20,53 +24,62 @@ namespace PromoWeb
             lblErrorDireccion.Text = string.Empty;
             try
             {
-                if (string.IsNullOrWhiteSpace(txtBoxDNI.Text) || !long.TryParse(txtBoxDNI.Text, out _))
+                if(!ValidarCampos())
                 {
-                    lblErrorDireccion.Text = "Por favor, ingresa un DNI válido.";
                     return;
-                }
-                if (string.IsNullOrWhiteSpace(txtBoxNombre.Text) || !txtBoxNombre.Text.All(char.IsLetter))
-                {
-                    lblErrorDireccion.Text = "Por favor, ingresa un nombre válido.";
-                    return;
-                }
-                if (string.IsNullOrWhiteSpace(txtBoxApellido.Text) || !txtBoxApellido.Text.All(char.IsLetter))
-                {
-                    lblErrorDireccion.Text = "Por favor, ingresa un apellido válido.";
-                    return;
-                }
-                if (string.IsNullOrWhiteSpace(txtBoxEmail.Text) || !validacionEmail(txtBoxEmail.Text))
-                {
-                    lblErrorDireccion.Text = "Por favor, ingresa un email válido.";
-                    return;
-                }
-                if (string.IsNullOrWhiteSpace(txtBoxDireccion.Text))
-                {
-                    lblErrorDireccion.Text = "Por favor, ingresa una dirección.";
-                    return;
-                }
-                if (string.IsNullOrWhiteSpace(txtBoxCP.Text) || !int.TryParse(txtBoxCP.Text, out _))
-                {
-                    lblErrorDireccion.Text = "Por favor, ingresa un código postal válido.";
-                    return;
-                }
-                if (!chkBoxTerms.Checked)
-                {
-                    lblErrorDireccion.Text = "Debes aceptar los términos y condiciones.";
-                    return;
-                }
+                }    
             }
             catch (Exception ex)
             {
                 lblErrorDireccion.Text = "Ocurrió un error: " + ex.Message;
             }
         }
-        private bool validacionEmail(string email)
+        private bool ValidarCampos()
+        {
+            if (string.IsNullOrWhiteSpace(txtBoxDNI.Text) || !long.TryParse(txtBoxDNI.Text, out _) || txtBoxDNI.Text.Length != 8)
+            {
+                lblErrorDireccion.Text = "Por favor, ingresa un DNI válido de 8 dígitos y que no sea letras.";
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtBoxNombre.Text) || !txtBoxNombre.Text.All(char.IsLetter))
+            {
+                lblErrorDireccion.Text = "Por favor, ingresa un nombre válido.";
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtBoxApellido.Text) || !txtBoxApellido.Text.All(char.IsLetter))
+            {
+                lblErrorDireccion.Text = "Por favor, ingresa un apellido válido.";
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtBoxEmail.Text) || !ValidacionEmail(txtBoxEmail.Text))
+            {
+                lblErrorDireccion.Text = "Por favor, ingresa un email válido.";
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtBoxDireccion.Text))
+            {
+                lblErrorDireccion.Text = "Por favor, ingresa una dirección.";
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtBoxCP.Text) || !int.TryParse(txtBoxCP.Text, out _))
+            {
+                lblErrorDireccion.Text = "Por favor, ingresa un código postal válido.";
+                return false;
+            }
+            if (!chkBoxTerms.Checked)
+            {
+                lblErrorDireccion.Text = "Debes aceptar los términos y condiciones.";
+                return false;
+            }
+
+            return true;
+        }
+        private bool ValidacionEmail(string email)
         {
             try
             {
-                var addr = new System.Net.Mail.MailAddress(email);
-                return addr.Address == email;
+                string aux = new MailAddress(email).Address;
+                return aux == email;
             }
             catch
             {
